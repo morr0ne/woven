e6fce7ba729a9166f6387601e864e97054469994
Matilde Morrone <contact@morrone.dev>
2023-10-27 15:42:02 +0200

    Fix comment grammar

--- system/src/clear.rs
+++ system/src/clear.rs
@@ -22,7 +22,7 @@ FIXME
 
 The current implementation simply writes e couple of ansi escape codes to stdout.
 Is that right? It does work but it doesn't feel like it's the correct solution.
-Perhaps it is and other programs are just overengineered, or perhaps I'm just stupid. Who can say.
+Perhaps it is and other programs are just overengineered, or perhaps I'm just stupid. Who can say? Maybe its both.
 */
 fn main() -> i32 {
     let stdout = unsafe { stdout() };
