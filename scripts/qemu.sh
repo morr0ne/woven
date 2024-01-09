a36ab748bf8753d0fa2209ee27ea7eb384164fd7
Matilde Morrone <contact@morrone.dev>
2024-01-09 16:24:59 +0100

    Remove serial from qemu

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -3,6 +3,5 @@
 qemu-system-x86_64 -m 1G -cdrom cutiepie.iso -boot d \
   -enable-kvm -machine q35 -device intel-iommu -cpu host \
   -nic user,model=virtio-net-pci -vga std -smp 6 \
-  -serial stdio \
   -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2-ovmf/x64/OVMF_CODE.fd \
   -drive if=pflash,format=raw,file=OVMF_VARS.fd
