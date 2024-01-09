398b486c05267ca9a2b6f60037c9cecda41ff8e9
Federico Maria Morrone <contact@morrone.dev>
2024-01-09 15:16:41 +0100

    Updated qemu scripts

--- scripts/qemu_spice.sh
+++ scripts/qemu_spice.sh
@@ -3,6 +3,7 @@
 qemu-system-x86_64 -m 256M -cdrom cutiepie.iso -boot d \
   -enable-kvm -machine q35 -device intel-iommu -cpu host \
   -nic user,model=virtio-net-pci -vga std -smp 6 \
-  -serial stdio
+  -device virtio-serial-pci -spice port=5930,disable-ticketing=on \
+  -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 -chardev spicevmc,id=spicechannel0,name=vdagent
 # -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2-ovmf/x64/OVMF_CODE.fd \
 # -drive if=pflash,format=raw,file=OVMF_VARS.fd
