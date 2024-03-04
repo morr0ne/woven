a110b3c8b17df3ac767ac4f913c1645341d3a59e
Matilde Morrone <contact@morrone.dev>
2024-03-04 13:06:42 +0100

    Switch to completely custom rootfs

--- system/src/raminit.rs
+++ system/src/raminit.rs
@@ -5,13 +5,23 @@
 #![feature(core_intrinsics)]
 #![feature(naked_functions)]
 
+// TODO: comment everything
+
+use core::{ffi::c_char, ptr::null};
+
 use rustix::{
-    fs::{mkdir, Mode},
+    fs::{mkdir, open, Mode, OFlags},
     io::Result,
-    mount::{mount_move, mount_none},
-    process::getpid,
-    runtime::{execve, fork, Fork},
+    ioctl::ioctl,
+    mount::{mount2, mount_move, mount_none, MountFlags},
+    path::Arg,
+    process::{chdir, chroot, getpid},
+    runtime::execve,
 };
+
+mod loop_configure;
+
+use loop_configure::ConfigureLoop;
 
 #[panic_handler]
 fn panic(_panic: &core::panic::PanicInfo<'_>) -> ! {
@@ -19,15 +29,40 @@ fn panic(_panic: &core::panic::PanicInfo<'_>) -> ! {
 }
 
 fn main() -> i32 {
+    // Make sure we actually are pid 1 otherwise stuff will go wrong
     if !getpid().is_init() {
-        return 1;
+        return -1;
     }
 
+    // Mount everything
+    // TODO: should allow mounting a specific root
     if let Err(err) = mount_system() {
         return err.raw_os_error();
     }
 
+    // Switch to the real mount
+    if let Err(err) = switch_root() {
+        return err.raw_os_error();
+    }
+
     0
+}
+
+fn switch_root() -> Result<()> {
+    chdir(c"/stem")?;
+
+    // TODO: delete everything before mounting
+
+    mount_move(c".", c"/")?;
+
+    chroot(c".")?;
+    chdir(c"/")?;
+
+    let argv: &[*const c_char] = &[c"/sbin/init".as_ptr(), null()];
+
+    let err = unsafe { execve(c"/sbin/init", argv.as_ptr(), null()) };
+
+    return Err(err);
 }
 
 fn mount_system() -> Result<()> {
@@ -40,19 +75,42 @@ fn mount_system() -> Result<()> {
 
     mount_none(c"/dev/pts", c"devpts", None)?;
 
-    // TODO: mount a real fs like squashfs or erofs
-    mount_none(c"/mnt", c"tmpfs", None)?;
+    mkdir(c"/isomount", Mode::empty())?;
+    mount2(
+        Some(c"/dev/sr0"),
+        c"/isomount",
+        Some(c"iso9660"),
+        MountFlags::RDONLY,
+        None,
+    )?;
 
-    // Critical file system folders
-    mkdir(c"/mnt/dev", Mode::empty())?;
-    mkdir(c"/mnt/sys", Mode::empty())?;
-    mkdir(c"/mnt/proc", Mode::empty())?;
-    mkdir(c"/mnt/tmp", Mode::empty())?;
+    loop_device(c"/isomount/stemfs.erofs")?;
 
-    mount_move(c"/dev", c"/mnt/dev")?;
-    mount_move(c"/sys", c"/mnt/sys")?;
-    mount_move(c"/proc", c"/mnt/proc")?;
-    mount_move(c"/tmp", c"/mnt/tmp")?;
+    mount2(
+        Some(c"/dev/loop0"),
+        c"/stem",
+        Some(c"erofs"),
+        MountFlags::RDONLY,
+        None,
+    )?;
+
+    mount_move(c"/dev", c"/stem/dev")?;
+    mount_move(c"/sys", c"/stem/sys")?;
+    mount_move(c"/proc", c"/stem/proc")?;
+    mount_move(c"/tmp", c"/stem/tmp")?;
+
+    Ok(())
+}
+
+fn loop_device<P: Arg>(file: P) -> Result<()> {
+    let file = open(file, OFlags::RDONLY, Mode::empty())?;
+
+    let loop_device = open(c"/dev/loop0", OFlags::RDWR, Mode::empty())?;
+
+    let configure_loop = ConfigureLoop::new(file);
+
+    // FIXME: handle errors
+    let _output = unsafe { ioctl(&loop_device, configure_loop) }?;
 
     Ok(())
 }
