67c2cb89127facf0af48c97a534a4f0f8e0a00b4
Federico Maria Morrone <contact@morrone.dev>
2024-03-10 18:55:43 +0100

    Improve kernel config and enable hardware acceleration

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -2,6 +2,7 @@
 
 qemu-system-x86_64 -m 1G -cdrom woven.iso -boot d \
   -enable-kvm -machine q35 -device intel-iommu -cpu host \
-  -nic user,model=virtio-net-pci -vga std -smp 6 \
+  -nic user,model=virtio-net-pci -smp 6 \
   -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2-ovmf/x64/OVMF_CODE.fd \
-  -drive if=pflash,format=raw,file=OVMF_VARS.fd
+  -drive if=pflash,format=raw,file=OVMF_VARS.fd \
+  -device virtio-vga-gl -display gtk,gl=on
