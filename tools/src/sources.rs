3b198344ab8c1b0d87c0dfca4d2ec7133d118cd6
Federico Maria Morrone <contact@morrone.dev>
2024-04-14 18:06:35 +0200

    Change sources format

--- tools/src/sources.rs
+++ tools/src/sources.rs
@@ -9,7 +9,6 @@ use blake3::Hasher;
 use bzip2::read::BzDecoder;
 use camino::Utf8Path;
 use flate2::read::GzDecoder;
-use indexmap::IndexMap;
 use indicatif::ProgressBar;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
@@ -17,7 +16,13 @@ use tar::Archive;
 use xz2::read::XzDecoder;
 
 #[derive(Debug, Deserialize, Serialize)]
+struct Config {
+    sources: Vec<Source>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 struct Source {
+    name: String,
     url: String,
     target: String,
     version: String,
@@ -27,12 +32,25 @@ struct Source {
 #[tokio::main]
 async fn main() -> Result<()> {
     let file = fs::read_to_string("sources.toml")?;
-    let sources: IndexMap<String, Source> = toml::from_str(&file)?;
+    let config: Config = toml::from_str(&file)?;
 
     let args: Vec<String> = std::env::args().collect();
 
     if let Some(source) = args.get(1) {
-        print!("{}", sources[source].version);
+        print!(
+            "{}",
+            config
+                .sources
+                .iter()
+                .find_map(|src| {
+                    if &src.name == source {
+                        Some(src.version.as_str())
+                    } else {
+                        None
+                    }
+                })
+                .unwrap_or("unknown")
+        );
         return Ok(());
     }
 
@@ -40,9 +58,9 @@ async fn main() -> Result<()> {
 
     let client = Client::new();
 
-    for (name, source) in &sources {
-        download_source(&client, name, source).await?;
-        extract_source(name, source).await?;
+    for source in &config.sources {
+        download_source(&client, source).await?;
+        extract_source(source).await?;
     }
 
     println!("Sources OK");
@@ -50,7 +68,7 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-async fn download_source(client: &Client, name: &str, source: &Source) -> Result<()> {
+async fn download_source(client: &Client, source: &Source) -> Result<()> {
     let target_path: String = format!("sources/tar/{}", source.target);
 
     if Path::new(&target_path).exists() && hash_path(&target_path)? == source.hash {
@@ -59,7 +77,7 @@ async fn download_source(client: &Client, name: &str, source: &Source) -> Result
 
     let mut target = File::create(target_path)?;
 
-    println!("Downloading {name}...");
+    println!("Downloading {}...", source.name);
 
     let mut res = client.get(&source.url).send().await?;
     let len = res.content_length().unwrap_or(0);
@@ -84,11 +102,11 @@ fn hash_path(path: &str) -> io::Result<String> {
     Ok(hash.to_hex().to_string())
 }
 
-async fn extract_source(name: &str, source: &Source) -> Result<()> {
+async fn extract_source(source: &Source) -> Result<()> {
     let target_path = format!("sources/tar/{}", source.target);
     let target_path = Utf8Path::new(&target_path);
 
-    let archive_path = format!("sources/{name}");
+    let archive_path = format!("sources/{}", source.name);
     let archive_path = Utf8Path::new(&archive_path);
 
     if archive_path.join(".ok").exists() {
@@ -103,13 +121,13 @@ async fn extract_source(name: &str, source: &Source) -> Result<()> {
 
     match target_path.extension().unwrap() {
         "xz" => {
-            unpack_archive(XzDecoder::new(target), name)?;
+            unpack_archive(XzDecoder::new(target), &source.name)?;
         }
         "gz" => {
-            unpack_archive(GzDecoder::new(target), name)?;
+            unpack_archive(GzDecoder::new(target), &source.name)?;
         }
         "bz2" => {
-            unpack_archive(BzDecoder::new(target), name)?;
+            unpack_archive(BzDecoder::new(target), &source.name)?;
         }
         _ => bail!("Something went wrong extracting"),
     }
