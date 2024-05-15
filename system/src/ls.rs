f768215617149236f84db08d3c8afe7f5bf1b250
Federico Maria Morrone <contact@morrone.dev>
2024-05-15 14:51:44 +0200

    Add ls command

---
+++ system/src/ls.rs
@@ -0,0 +1,33 @@
+#![no_std]
+#![no_main]
+#![allow(internal_features)]
+#![feature(lang_items)]
+#![feature(core_intrinsics)]
+#![feature(naked_functions)]
+
+fn main() -> i32 {
+    0
+}
+
+#[naked]
+#[no_mangle]
+unsafe extern "C" fn _start() -> ! {
+    use core::arch::asm;
+
+    fn entry() -> ! {
+        rustix::runtime::exit_group(main())
+    }
+
+    asm!(
+        "mov rdi, rsp", // Pass the incoming `rsp` as the arg to `entry`.
+        "push rbp",     // Set the return address to zero.
+        "jmp {entry}",  // Jump to `entry`.
+        entry = sym entry,
+        options(noreturn),
+    );
+}
+
+#[panic_handler]
+fn panic(_panic: &core::panic::PanicInfo<'_>) -> ! {
+    core::intrinsics::abort()
+}
