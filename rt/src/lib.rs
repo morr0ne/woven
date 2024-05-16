345e3cdd3e5a6ab009607d7cfcdcf78d524a8253
Federico Maria Morrone <contact@morrone.dev>
2024-05-16 21:50:11 +0200

    Add argument parsing

--- rt/src/lib.rs
+++ rt/src/lib.rs
@@ -5,7 +5,12 @@
 #![feature(core_intrinsics)]
 #![feature(naked_functions)]
 
-use core::arch::asm;
+use core::{
+    arch::asm,
+    ffi::{c_int, c_uint, CStr},
+    ptr::null_mut,
+    sync::atomic::{AtomicI32, AtomicPtr, Ordering},
+};
 
 use rustix::{io::Errno, process::EXIT_SUCCESS};
 use rustix_dlmalloc::GlobalDlmalloc;
@@ -53,13 +58,28 @@ impl<T: Termination, E: Termination> Termination for Result<T, E> {
     }
 }
 
+static ARGC: AtomicI32 = AtomicI32::new(0);
+static ARGV: AtomicPtr<*const u8> = AtomicPtr::new(null_mut());
+static ENVP: AtomicPtr<*const u8> = AtomicPtr::new(null_mut());
+
 #[naked]
 #[no_mangle]
 pub unsafe extern "C" fn _start() -> ! {
-    fn entry() -> ! {
+    fn entry(mem: *mut usize) -> ! {
         extern "Rust" {
             fn main() -> i32;
         }
+
+        unsafe {
+            let kernel_argc = *mem;
+            let argc = kernel_argc as c_int;
+            let argv = mem.add(1).cast::<*mut u8>();
+            let envp = argv.add(argc as c_uint as usize + 1);
+
+            ARGC.store(argc, Ordering::Relaxed);
+            ARGV.store(argv as *mut _, Ordering::Relaxed);
+            ENVP.store(envp as *mut _, Ordering::Relaxed);
+        };
 
         rustix::runtime::exit_group(unsafe { main() })
     }
@@ -71,4 +91,40 @@ pub unsafe extern "C" fn _start() -> ! {
         entry = sym entry,
         options(noreturn),
     );
+}
+
+pub fn args() -> Args {
+    Args { index: 0 }
+}
+
+pub struct Args {
+    index: usize,
+}
+
+impl Iterator for Args {
+    type Item = &'static CStr;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let argv = ARGV.load(Ordering::Relaxed);
+        let argc = ARGC.load(Ordering::Relaxed);
+
+        if self.index < argc as usize {
+            unsafe {
+                let arg = argv.add(self.index);
+                self.index += 1;
+
+                Some(CStr::from_ptr(arg.read().cast()))
+            }
+        } else {
+            None
+        }
+    }
+}
+
+impl ExactSizeIterator for Args {
+    fn len(&self) -> usize {
+        let argc = ARGC.load(Ordering::Relaxed);
+
+        argc as usize - self.index
+    }
 }
